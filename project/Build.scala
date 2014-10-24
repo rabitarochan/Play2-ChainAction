916a50a71db11f7a45db66466a47214cf4c29b82
rabitarochan <rabitarochan@gmail.com>
2014-10-25 01:18:54 +0900

    Add publish settings.

--- project/Build.scala
+++ project/Build.scala
@@ -4,10 +4,49 @@ import Keys._
 import play.PlayScala
 
 object Play2ChainActionBuild extends Build {
+  val _name         = "Play2-ChainAction"
+  val _organization = "com.github.rabitarochan"
+  val _version      = "0.1"
+  val _scalaVersion = "2.11.2"
 
-  val baseSettings = Seq(
-    organization := "com.github.rabitarochan",
-    version := "0.1-SNAPSHOT"
+  lazy val baseSettings = Seq(
+    organization := _organization,
+    version := _version
+  )
+
+  lazy val publishSettings = Seq(
+    publishMavenStyle := true,
+    publishTo <<= version { (v: String) =>
+      val nexus = "https://oss.sonatype.org/"
+      if (v.trim.endsWith("SNAPSHOT")) {
+        Some("snapshots" at nexus + "content/repositories/snapshots")
+      } else {
+        Some("releases" at nexus + "service/local/staging/deploy/maven2")
+      }
+    },
+    publishArtifact in Test := false,
+    pomIncludeRepository := { _ => false },
+    pomExtra := (
+      <url>https://github.com/rabitarochan/Play2-ChainAction</url>
+      <licenses>
+        <license>
+          <name>Apache License, Version 2.0</name>
+          <url>http://www.apache.org/licenses/LICENSE-2.0</url>
+          <distribution>repo</distribution>
+        </license>
+      </licenses>
+      <scm>
+        <url>git@github.com:rabitarochan/Play2-ChainAction.git</url>
+        <connection>scm:git:git@github.com:rabitarochan/Play2-ChainAction.git</connection>
+      </scm>
+      <developers>
+        <developer>
+          <id>rabitarochan</id>
+          <name>Kengo Asamizu</name>
+          <url>https://github.com/rabitarochan</url>
+        </developer>
+      </developers>
+    )
   )
 
   lazy val root = Project("root", file("."))
@@ -16,9 +55,10 @@ object Play2ChainActionBuild extends Build {
 
   lazy val core = Project("core", file("core"))
     .settings(baseSettings : _*)
+    .settings(publishSettings : _*)
     .settings(
-      name := "Play2-ChainAction",
-      scalaVersion := "2.11.2",
+      name := _name,
+      scalaVersion := _scalaVersion,
       libraryDependencies ++= Seq(
         "com.typesafe.play" %% "play" % "2.3.4" % "provided"
       )
@@ -27,7 +67,7 @@ object Play2ChainActionBuild extends Build {
   lazy val example = Project("example", file("example"))
     .enablePlugins(PlayScala)
     .settings(
-      scalaVersion := "2.11.2",
+      scalaVersion := _scalaVersion,
       libraryDependencies ++= Seq(
         "com.typesafe.play" %% "play" % "2.3.4" % "provided"
       )
